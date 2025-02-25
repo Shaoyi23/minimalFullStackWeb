31f6c984165de0abc9f954cf133eec263dbb3133
Lando <1293471579@qq.com>
2025-02-25 20:16:22 +0800

    update file

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,12 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
